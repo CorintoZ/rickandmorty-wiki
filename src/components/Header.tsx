9b1971726908ccb8622d6edabf642502f2639289
CorintoZ <david.moreno92@gmail.com>
2023-02-03 17:18:35 +0100

    Display logout option

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 const Header = () => {
+  const cookies = new Cookies();
+  const token = cookies.get('TOKEN');
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    cookies.remove('TOKEN', { path: '/' });
+    navigate(0);
+  };
+
   return (
     <div className="bg-blue-500 h-14 w-full flex justify-end items-center gap-x-4 p-2 text-white text-lg font-medium">
-      <Link to={'/login'}>Login</Link>
+      {token ? (
+        <button
+          className="p-2 tex-white bg-red-400 rounded-md"
+          type="submit"
+          onClick={logOut}
+        >
+          Logout
+        </button>
+      ) : (
+        <Link to={'/login'}>Login</Link>
+      )}
       <Link to={'/register'}>Register</Link>
     </div>
   );
